f595a269c414224a147ede4a0d0adc134ce9a715
Aleksandar Miljusevic <miljusevic@yahoo.com>
2011-05-14 19:35:39 -0400

    Fixed DAO initialization. Set creation time if it wasn't set. Added logger.

--- middleware/src/main/java/com/libereco/server/service/impl/UserServiceImpl.java
+++ middleware/src/main/java/com/libereco/server/service/impl/UserServiceImpl.java
@@ -5,7 +5,12 @@
  */
 package com.libereco.server.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.Timestamp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.libereco.server.dao.UserDao;
 import com.libereco.server.dto.UserDto;
@@ -17,46 +22,74 @@ import com.libereco.server.service.UserService;
  * @author Aleksandar
  *
  */
-
+@Service("userService")
+@Transactional
 public class UserServiceImpl extends AbstractBasicService<Long, User, UserDto> implements UserService {
 	
-	@Autowired
-	private UserDao userDao;
+	private static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+	//	@Autowired
+//	private UserDao userDao;
+	
+	public UserServiceImpl(UserDao userDao) {
+		super(userDao);
+	}
+
+	public UserDao getUserDao() {
+		return (UserDao) getMainDao();
+	}
+
+	public void setUserDao(UserDao userDao) {
+		setMainDao(userDao);
+	}
 
 	@Override
 	public void addUser(UserDto userDto) {
+		getLogger().debug("Add user [" + userDto + "]");
+				
+		// TODO: Add validations
+		
+		if (userDto.getCreated() == null) {
+			userDto.setCreated(new Timestamp(System.currentTimeMillis()));
+		}
+		
 		saveOrUpdate(userDto);
 	}
 
 	@Override
 	public void updateUser(UserDto userDto) {
+		getLogger().debug("Update user [" + userDto + "]");
+		
+		// TODO: Add validations
+		userDto.setLastUpdated(new Timestamp(System.currentTimeMillis()));
 		saveOrUpdate(userDto);		
 	}
 
 	@Override
 	public void deleteUser(UserDto userDto) {
 		User userEntity = toEntity(userDto);
+		
 		// TODO: Update status to use enum type. This is just for the prototype.
 		userEntity.setStatus(UserConstants.UserStatus.DELETED);
-		userDao.saveOrUpdate(userEntity);
+		getUserDao().saveOrUpdate(userEntity);
 	}
 
 	@Override
 	public void deleteUser(String userName) {
-		User userEntity = userDao.findByUserName(userName);
+		User userEntity = getUserDao().findByUserName(userName);
 		userEntity.setStatus(UserConstants.UserStatus.DELETED);		
-		userDao.saveOrUpdate(userEntity);
+		getUserDao().saveOrUpdate(userEntity);
 	}
 
 	@Override
 	public boolean hasUser(String userName) {
-		return userDao.hasUserName(userName);
+		return getUserDao().hasUserName(userName);
 	}
 	
 	@Override
 	public UserDto getUser(String userName, boolean activeOnly) {
 		UserDto userDto = null;
-		User userEntity = userDao.findByUserName(userName);
+		User userEntity = getUserDao().findByUserName(userName);
 
 		if (activeOnly && userEntity != null) {
 			// TODO: Update status to use enum type. This is just for the prototype.
@@ -70,6 +103,10 @@ public class UserServiceImpl extends AbstractBasicService<Long, User, UserDto> i
 	}
 
 	public UserDto get(Long id) {
-		return toDto(userDao.findById(id));
+		return toDto(getUserDao().findById(id));
+	}
+	
+	protected Logger getLogger() {
+		return logger;
 	}
 }
